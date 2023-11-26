bf89f208f521a9f7ef066ca324f124cb49f9a17d
ShanaDart73 <carole.kayungu@gmail.com>
2023-11-26 19:17:35 +0100

    worked on homepage layout

--- app/page.js
+++ app/page.js
@@ -1,5 +1,39 @@
-import Image from 'next/image'
+//import Image from 'next/image'
 
 export default function Home() {
-    return <h1>Hello, Next.js!</h1>
+    const big_header = "next.js"
+    const small_header = "next.js"
+    const about = "Next.JS Portfolio"
+    return (
+        <div className="container">
+            <header>
+                <div className="sphere">
+                    <p>{small_header}</p>
+                    <h1>{big_header}</h1>
+                    <p>{small_header}</p>
+                </div>
+            </header>
+
+            <div>
+                <section className="one">
+                    <div className="wrapper">Fibonacci Series</div>
+                </section>
+                <section className="two">
+                    <div className="wrapper">Palindrome Number</div>
+                </section>
+                <section className="three">
+                    <div className="wrapper">Armstrong Number</div>
+                </section>
+                <section className="four">
+                    <div className="wrapper">Pattern With Numbers</div>
+                </section>
+            </div>
+
+            <footer>
+                <div>
+                    <p>{about}</p>
+                </div>
+            </footer>
+        </div>
+    )
 }
