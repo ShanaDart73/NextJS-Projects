881fd23ee455f11f22ac483db4fd44b0b9fca8c3
ShanaDart73 <carole.kayungu@gmail.com>
2024-05-30 13:00:18 +0200

    configured prettier

--- app/page.js
+++ app/page.js
@@ -1,45 +1,42 @@
-import Link from 'next/link'
-
-const fibonacci = "In fibonacci series, the sum of two adjacent numbers is equal to the following number. For example: 0, 1, 1, 2, 3, 5, etc."
+import Link from "next/link";
 
 export default function Home() {
-    const big_header = "next.js"
-    const small_header = "next.js"
-    const about = "Next.JS Portfolio"
+  const big_header = "next.js";
+  const small_header = "next.js";
+  const about = "Next.JS Portfolio";
 
-    return (
-        <div className="container">
-            <header>
-                <div className="sphere">
-                    <p>{small_header}</p>
-                    <h1>{big_header}</h1>
-                    <p>{small_header}</p>
-                </div>
-            </header>
-
-            <div>
-                <section className="one">
-                    <div className="wrapper">
-                        <Link href="pages/projects/fibonacci">Fibonacci Series</Link>
-                        <p>{fibonacci}</p>
-                    </div>
-                </section>
-                <section className="two">
-                    <div className="wrapper">Palindrome Number</div>
-                </section>
-                <section className="three">
-                    <div className="wrapper">Armstrong Number</div>
-                </section>
-                <section className="four">
-                    <div className="wrapper">Pattern With Numbers</div>
-                </section>
-            </div>
-
-            <footer>
-                <div>
-                    <p>{about}</p>
-                </div>
-            </footer>
+  return (
+    <div className="container">
+      <header>
+        <div className="sphere">
+          <p>{small_header}</p>
+          <h1>{big_header}</h1>
+          <p>{small_header}</p>
         </div>
-    )
+      </header>
+
+      <div>
+        <section className="one">
+          <div className="wrapper">
+            <Link href="pages/projects/e-commerce">e-commerce</Link>
+          </div>
+        </section>
+        <section className="two">
+          <div className="wrapper">Palindrome Number</div>
+        </section>
+        <section className="three">
+          <div className="wrapper">Armstrong Number</div>
+        </section>
+        <section className="four">
+          <div className="wrapper">Pattern With Numbers</div>
+        </section>
+      </div>
+
+      <footer>
+        <div>
+          <p>{about}</p>
+        </div>
+      </footer>
+    </div>
+  );
 }
---
+++ app/pages/projects/e-commerce/page.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+const Page = () => {
+  return (
+    <div className="content">
+      <nav>
+        <Link href="/">Home</Link>
+      </nav>
+      <div>
+        <h2>e-commerce</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
