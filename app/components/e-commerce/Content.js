711af9199675949b93e3597759cdcdc2d2c36fb2
ShanaDart73 <carole.kayungu@gmail.com>
2024-06-09 00:47:36 +0200

    fixed video size bugs

--- app/components/e-commerce/Content.js
+++ app/components/e-commerce/Content.js
@@ -4,15 +4,15 @@ const Content = () => {
   return (
     <div className="e-com-content">
       <h1>Content goes here</h1>
-      {/*<di>
-            <video
-                src={require("../../../public/africaFruit.mp4")}
-                autoPlay
-                muted
-                loop
-                style={{width: "100vw"}}
-            />
-        </di>*/}
+      <di>
+        <video
+          src={require("../../../public/africaFruit.mp4")}
+          autoPlay
+          muted
+          loop
+          style={{ width: "100vw" }}
+        />
+      </di>
     </div>
   );
 };
