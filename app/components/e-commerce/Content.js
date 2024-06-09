cc1048a5b41e31c108340a2e1415c221cf4a7da3
ShanaDart73 <carole.kayungu@gmail.com>
2024-06-09 18:39:01 +0200

    styled menu component

--- app/components/e-commerce/Content.js
+++ app/components/e-commerce/Content.js
@@ -3,16 +3,31 @@ import "/app/styles/e-commerce/style.css";
 const Content = () => {
   return (
     <div className="e-com-content">
-      <h1>Content goes here</h1>
-      <di>
-        <video
-          src={require("../../../public/africaFruit.mp4")}
-          autoPlay
-          muted
-          loop
-          style={{ width: "100vw" }}
-        />
-      </di>
+      <div>
+        <div className="promo">
+          <p>Sale! Up to 50% off</p>
+          <h1>Delicious fruit</h1>
+          <h2>from Africa</h2>
+        </div>
+        <di>
+          <video
+            src={require("../../../public/africaFruit.mp4")}
+            autoPlay
+            muted
+            loop
+            style={{ width: "100vw" }}
+          />
+        </di>
+      </div>
+      <div>
+        <div></div>
+      </div>
+      <div>
+        <div></div>
+      </div>
+      <div>
+        <div></div>
+      </div>
     </div>
   );
 };
