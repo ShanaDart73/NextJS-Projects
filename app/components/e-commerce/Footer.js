d07cbc21a23f1c84477521bc4f209d7c47ba5da5
ShanaDart73 <carole.kayungu@gmail.com>
2024-07-23 14:23:10 +0200

    adjusted footer layout

--- app/components/e-commerce/Footer.js
+++ app/components/e-commerce/Footer.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <div className=" py-10 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 bg-gray-100 text-sm mt-24">
+    <div className=" py-10 px-4 md:px-8 lg:px-10 xl:px-20 2xl:px-32 bg-gray-100 text-sm mt-24">
       <div className="flex flex-col md:flex-row justify-between gap-24">
         <div className="w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8">
           <Link href="/">
@@ -105,11 +105,11 @@ const Footer = () => {
           </div>
           <span className="font-semibold">Secure Payments</span>
           <div className=" flex justify-between">
-            <Image src="/mastercard.png" alt="" width={48} height={48} />
-            <Image src="/visa.png" alt="" width={48} height={48} />
-            <Image src="/americaexpress.png" alt="" width={48} height={48} />
-            <Image src="/discover.png" alt="" width={48} height={48} />
-            <Image src="/dinersclub.png" alt="" width={48} height={48} />
+            <Image src="/mastercard.png" alt="" width={40} height={20} />
+            <Image src="/visa.png" alt="" width={40} height={20} />
+            <Image src="/americaexpress.png" alt="" width={40} height={20} />
+            <Image src="/discover.png" alt="" width={40} height={20} />
+            <Image src="/dinersclub.png" alt="" width={40} height={20} />
           </div>
         </div>
       </div>
@@ -121,7 +121,7 @@ const Footer = () => {
           <span className="font-medium">Hungary | English</span>
           <div className="">
             <span className="text-gray-500 mr-4">Currency</span>
-            <span className="font-medium">$ USD</span>
+            <span className="font-medium">Ft</span>
           </div>
         </div>
       </div>
